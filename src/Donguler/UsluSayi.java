076bd021d7803c739f15a4bfb545008da2696f91
Salih <bayraktar.salih@hotmail.com>
2021-06-16 14:01:15 +0300

    Dongu Odevleri Eklendi

--- src/Donguler/UsluSayi.java
+++ src/Donguler/UsluSayi.java
@@ -1,10 +1,23 @@
 package Donguler;
 
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class UsluSayi {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Lutfen ussu alınacak sayıyı giriniz :");
+        int number = scanner.nextInt();
+
+        System.out.println("Lutfen hesaplamak istediginiz kuvvetin degerini giriniz : ");
+        int pow = scanner.nextInt();
+
+        System.out.println("Sayının üstü : " +
+                IntStream.range(0, pow)
+                        .reduce(1, (result, i) -> result *= number));
 
     }
 }
